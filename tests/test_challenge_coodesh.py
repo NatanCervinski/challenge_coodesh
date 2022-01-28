9f29238ad13c25a0e878126e8077f126a6f63e06
NatanCervinski <natanscer@gmail.com>
2022-01-28 19:08:33 -0300

    test for the first endpoint

--- tests/test_challenge_coodesh.py
+++ tests/test_challenge_coodesh.py
@@ -1,5 +1,17 @@
 from challenge_coodesh import __version__
+from fastapi.testclient import TestClient
+from challenge_coodesh.main import app
+
+client = TestClient(app)
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Back-end Challenge 2021 :medal: - Space Flight News"
+    }
